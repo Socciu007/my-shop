175b88d59aa3a0d2cff6eeccac696ea5e8691877
Money <manhtien310701@gmail.com>
2024-06-18 09:43:26 +0800

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"go_shop/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
